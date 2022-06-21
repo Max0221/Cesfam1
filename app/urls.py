8eb104ecc95d619c09d709049b38afa4078a1eb5
Max <80917688+Max0221@users.noreply.github.com>
2022-06-21 15:29:54 -0400

    lista pacientes lista medicamentos

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index,contacto,lista_productos,login
+from .views import index,contacto,lista_productos,login,lista_paciente,lista_medicamentos
 
 urlpatterns = [
     path('',index,  name="index"),
     path('contacto/',contacto,  name="contacto"),
     path('lista_productos/',lista_productos,  name="lista_producto"),
-    path('login/', login ,  name="login")
+    path('login/', login ,  name="login"),
+    path('lista_paciente/', lista_paciente ,  name="lista_paciente"),
+    path('lista_medicamentos/', lista_medicamentos, name="lista_medicamentos",)
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 import re
 from django.shortcuts import render
 from .models import medicamentocompra
+from .models import medicamento , fichapaciente
+
 
 # Create your views here.
 def index(request):
@@ -15,3 +17,16 @@ def lista_productos(request):
     return render(request, 'app/lista_productos.html' ,data)
 def login(request):
     return render(request, 'app/login.html' )
+def lista_paciente(request):
+    fichapacientes = fichapaciente.objects.all()
+    data = {
+        'fichapacientes' :fichapacientes
+    }
+    
+    return render(request, 'app/lista_paciente.html' ,data)
+def lista_medicamentos(request):
+    medicamentos = medicamento.objects.all()
+    data = {
+        'medicamentos' :medicamentos
+    }
+    return render(request, 'app/lista_medicamentos.html', data )
